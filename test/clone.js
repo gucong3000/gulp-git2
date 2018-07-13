acbaf08c9a4d2f7396907a9c08cfc2038dd438ad
尹珩力 <yinhengli@sina.com>
2018-07-13 23:07:47 +0800

    add clone

---
+++ test/clone.js
@@ -0,0 +1,26 @@
+"use strict";
+const path = require("path");
+const gitStream = require("..");
+const fixtures = path.join.bind(path, __dirname, "fixtures");
+const expect = require("chai").expect;
+const fs = require("fs-extra");
+
+describe("clone", () => {
+	const clear = async () => {
+		await Promise.all([
+			fs.remove(fixtures("clone")),
+		]);
+	};
+	after(clear);
+	beforeEach(clear);
+	if (process.env.CI) {
+		it("is clone success", async () => {
+			const git = gitStream({
+				gitWorkTree: fixtures("clone"),
+				gitDir: ".gitMock",
+			});
+			await git.clone("https://github.com/gucong3000/gulp-git2");
+			expect(await fs.exists(path.resolve(git.gitWorkTree, git.gitDir, "HEAD"))).to.be.true;
+		});
+	}
+});
